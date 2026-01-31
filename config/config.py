7be6a5e6845d349181eaa8ba48b23d8ca6d0dd8f
dtheo91 <dustintheobald@msn.com>
2026-01-31 14:53:24 +0100

    fix: azure ressource settings in config.py

--- config/config.py
+++ config/config.py
@@ -7,10 +7,10 @@ class Config(BaseSettings):
     # Azure Foundry
 
     # Azure ML
-    azure_subscription_id: str = "234a32e8-6f2e-47f8-8940-6204931f5ff5"
-    azure_resource_group: str = "rg_aifoundry_swe1"
-    azure_workspace_name: str = "wgg-swe-aml-ds"
-    azure_experiment_name: str = "ml-contracts-kommunikation"
+    azure_subscription_id: str = ""
+    azure_resource_group: str = ""
+    azure_workspace_name: str = ""
+    azure_experiment_name: str = ""
 
 
     # Load Environment
