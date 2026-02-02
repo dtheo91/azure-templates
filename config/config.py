e8cde576e5ca1b530e24b3c92a14d103e9a86bb3
dtheo91 <dustintheobald@msn.com>
2026-02-02 13:33:55 +0100

    fix: foundry client to call models

--- config/config.py
+++ config/config.py
@@ -5,13 +5,18 @@ import os
 
 class Config(BaseSettings):
     # Azure Foundry
-    user_endpoint: str
+    azure_foundry_endpoint: str
+    azure_foundry_openai_endpoint: str
+    azure_foundry_api_key: SecretStr
 
     # Azure ML
     azure_subscription_id: str
     azure_resource_group: str
     azure_workspace_name: str
     azure_experiment_name: str
+
+    # Azure Document Intelligence
+    azure_document_intelligence_endpoint: str
 
 
     # Load Environment
--- modules/azure_foundry/azure_foundry_client.py
+++ modules/azure_foundry/azure_foundry_client.py
@@ -1,11 +1,19 @@
 from azure.identity import DefaultAzureCredential
 from azure.ai.projects import AIProjectClient
+from openai import OpenAI
+from pydantic import SecretStr
 
 class AzureFoundryClient:
-    def __init__(self, user_endpoint: str):
+    def __init__(self, user_endpoint: str, openai_endpoint: str, api_key: SecretStr):
         self.project_client = AIProjectClient(
             endpoint=user_endpoint,
             credential=DefaultAzureCredential(),
         )
 
         self.openai_client = self.project_client.get_openai_client()
+        self.model_client = OpenAI(base_url=openai_endpoint, api_key=api_key.get_secret_value())
+
+    def get_embeddings(self, text_list: list, model_deployment_name: str):
+        """Generates vectors for a list of text strings."""
+        response = self.model_client.embeddings.create(input=text_list, model=model_deployment_name)
+        return [item.embedding for item in response.data]
